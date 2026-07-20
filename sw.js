62d598a210fe7ab09ed31451fba76320228f490a
Sunnysystemic <sondre0802@gmail.com>
2026-07-20 22:29:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,19 @@
-const CACHE = "okonomisk-fremtid-v12-reference-default";
+const CACHE = "okonomisk-fremtid-v13-plan-health";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./privacy.html"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.webmanifest",
-        "./icon-192.png",
-        "./icon-512.png"
-      ])
-    )
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
   );
+
   self.skipWaiting();
 });
 
@@ -19,32 +21,36 @@ self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.map(key => {
-          if (key !== CACHE) {
-            return caches.delete(key);
-          }
-        })
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
       )
     )
   );
+
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
+  if (event.request.method !== "GET") {
+    return;
+  }
 
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return (
-        cached ||
-        fetch(event.request).then(response => {
-          const copy = response.clone();
-          caches.open(CACHE).then(cache => {
-            cache.put(event.request, copy);
-          });
-          return response;
-        })
-      );
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then(networkResponse => {
+        const responseCopy = networkResponse.clone();
+
+        caches.open(CACHE).then(cache => {
+          cache.put(event.request, responseCopy);
+        });
+
+        return networkResponse;
+      });
     })
   );
 });
